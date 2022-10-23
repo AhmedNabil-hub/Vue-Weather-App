e0251f774009b8954cd9a0bd8956ce8f0926d0a1
Ahmed Nabil <a7med.nabil1655@gmail.com>
2022-10-23 07:55:53 +0200

    v.1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,152 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main :class="{ warm: weather.main && weather.main.temp > 16 }">
+    <div class="container">
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <div v-if="weather.main" class="weather-wrap">
+        <div class="location-box">
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ formatDate(weather.date) }}</div>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
 
+<script setup>
+import { computed, ref } from "vue";
+import moment from "moment";
+
+const api = ref({
+  api_key: "38382b734b5b641e41eb56b07198c351",
+  url_base: "https://api.openweathermap.org/data/2.5/",
+});
+
+const query = ref("");
+const weather = ref({});
+
+function fetchWeather(event) {
+  if (event.key == "Enter") {
+    fetch(
+      `${api.value.url_base}weather?q=${query.value}&units=metric&APPID=${api.value.api_key}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((results) => setResults(results));
+  }
+}
+
+function setResults(results) {
+  weather.value = results;
+}
+
+function formatDate(date) {
+  return moment().format('MMMM D YYYY, h:mm a');
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+main {
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
 }
 
-.logo {
+main.warm {
+  background-image: url("./assets/warm-bg.jpg");
+}
+
+.container {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
   display: block;
-  margin: 0 auto 2rem;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.location-box .date {
+  color: #fff;
+  font-size: 20;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
